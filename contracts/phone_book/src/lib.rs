c88683ce5186ad1890fc9d1e7d4ad5e4b5a69c74
blurbeast <leumasre@gmail>
2025-08-22 10:24:46 +0100

    done with phone book

--- contracts/phone_book/src/lib.rs
+++ contracts/phone_book/src/lib.rs
@@ -1,23 +1,78 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
+// use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
 
-#[contract]
-pub struct Contract;
+// #[contract]
+// pub struct Contract;
 
-// This is a sample contract. Replace this placeholder with your own contract logic.
-// A corresponding test example is available in `test.rs`.
-//
-// For comprehensive examples, visit <https://github.com/stellar/soroban-examples>.
-// The repository includes use cases for the Stellar ecosystem, such as data storage on
-// the blockchain, token swaps, liquidity pools, and more.
-//
-// Refer to the official documentation:
-// <https://developers.stellar.org/docs/build/smart-contracts/overview>.
-#[contractimpl]
-impl Contract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
-    }
-}
+// #[contractimpl]
+// impl Contract {
+//     pub fn hello(env: Env, to: String) -> Vec<String> {
+//         vec![&env, String::from_str(&env, "Hello"), to]
+//     }
+// }
+
+use soroban_sdk::{contract, contractimpl, contracttype, xdr::Uint256, Address, Env, String, U256};
 
 mod test;
+
+
+
+#[contracttype]
+pub enum PhoneBookKey {
+    Owner,
+    Name,
+    Contact(String,),
+}
+
+
+#[contracttype]
+#[derive(Debug)]
+pub struct ContactInfo {
+    pub name: String,
+    pub mobile_number: String,
+}
+
+#[contract]
+pub struct PhoneBook;
+
+
+#[contractimpl]
+impl PhoneBook {
+
+    pub fn __init(env: &Env, owner: Address, name: String) {
+        if env.storage().instance().has(&PhoneBookKey::Owner) {
+            // throw
+        }
+
+        env.storage().instance().set(&PhoneBookKey::Owner, &owner);
+        env.storage().instance().set(&PhoneBookKey::Name, &name);
+    }
+
+
+    pub fn add_contact(env: &Env, name: String, mobile_number: String) {
+
+        let contact_info = ContactInfo {
+            name: name.clone(),
+            mobile_number
+        };
+        env.storage().instance().set(&PhoneBookKey::Contact(name), &contact_info);
+    }
+
+    pub fn get_contact(env: Env, name: String) -> Option<ContactInfo> {
+        env.storage().instance().get(&PhoneBookKey::Contact(name))
+    }
+
+    pub fn remove_contact(env: Env, name: String) {
+        // Verify caller is the owner
+        let owner: Address = env.storage().instance().get(&PhoneBookKey::Owner)
+            .expect("Contract not initialized");
+        owner.require_auth();
+
+        let is_exist:bool = env.storage().instance().has(&PhoneBookKey::Contact(name.clone()));
+        if !is_exist {
+            
+        }
+        
+        env.storage().instance().remove(&PhoneBookKey::Contact(name));
+    }
+}
