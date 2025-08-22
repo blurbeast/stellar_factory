d775f8365f23f26d1daadbfda25193f99de31be0
blurbeast <leumasre@gmail>
2025-08-22 10:58:14 +0100

    done

--- contracts/phone/src/lib.rs
+++ contracts/phone/src/lib.rs
@@ -1,23 +1,46 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
+// use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
 
-#[contract]
-pub struct Contract;
+// #[contract]
+// pub struct Contract;
 
-// This is a sample contract. Replace this placeholder with your own contract logic.
-// A corresponding test example is available in `test.rs`.
-//
-// For comprehensive examples, visit <https://github.com/stellar/soroban-examples>.
-// The repository includes use cases for the Stellar ecosystem, such as data storage on
-// the blockchain, token swaps, liquidity pools, and more.
-//
-// Refer to the official documentation:
-// <https://developers.stellar.org/docs/build/smart-contracts/overview>.
-#[contractimpl]
-impl Contract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
-    }
-}
+// #[contractimpl]
+// impl Contract {
+//     pub fn hello(env: Env, to: String) -> Vec<String> {
+//         vec![&env, String::from_str(&env, "Hello"), to]
+//     }
+// }
+
+use soroban_sdk::{contract, contractimpl, contracttype, Address, Env, String};
 
 mod test;
+
+
+#[contract]
+pub struct Phone;
+
+
+
+#[contracttype]
+pub enum PhoneKey{
+    Owner,
+    OwnerDetails,
+}
+
+
+#[contractimpl]
+impl Phone {
+    
+    pub fn __init(env: Env, owner: Address, name: String) {
+        if env.storage().instance().has(&PhoneKey::Owner) {}
+
+        env.storage().instance().set(&PhoneKey::Owner, &owner);   
+        env.storage().instance().set(&PhoneKey::OwnerDetails, &(owner, name));
+    }
+
+
+    pub fn create_new_phone_book(env: Env) {
+        // env.deployer()
+        // env.register(contract, constructor_args)
+    }
+}
--- contracts/phone_book/src/test.rs
+++ contracts/phone_book/src/test.rs
@@ -1,21 +1,6 @@
-#![cfg(test)]
 
-// use super::*;
-// use soroban_sdk::{vec, Env, String};
 
-// #[test]
-// fn test() {
-//     let env = Env::default();
-//     let contract_id = env.register(Contract, ());
-//     let client = ContractClient::new(&env, &contract_id);
+#[cfg(test)]
+mod test {
 
-//     let words = client.hello(&String::from_str(&env, "Dev"));
-//     assert_eq!(
-//         words,
-//         vec![
-//             &env,
-//             String::from_str(&env, "Hello"),
-//             String::from_str(&env, "Dev"),
-//         ]
-//     );
-// }
+}
