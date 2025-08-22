ce2611c88a03a6f88fdb3fd6a8c214392b86f42d
blurbeast <leumasre@gmail>
2025-08-22 11:28:09 +0100

    formatted

--- contracts/phone/src/lib.rs
+++ contracts/phone/src/lib.rs
@@ -8,60 +8,81 @@ use phone_book::PhoneBookClient;
 pub struct Phone;
 
 #[contracttype]
-pub enum PhoneKey{
+pub enum PhoneKey {
     Owner,
     OwnerDetails,
     PhoneBookName(String),
 }
 
-
 #[contracttype]
-pub struct  ContactInfo {
+pub struct ContactInfo {
     pub name: String,
     pub no: String,
 }
 
-
 #[contractimpl]
 impl Phone {
-    
     pub fn __init(env: Env, owner: Address, name: String) {
         if env.storage().instance().has(&PhoneKey::Owner) {}
 
-        env.storage().instance().set(&PhoneKey::Owner, &owner);   
-        env.storage().instance().set(&PhoneKey::OwnerDetails, &(owner, name));
+        env.storage().instance().set(&PhoneKey::Owner, &owner);
+        env.storage()
+            .instance()
+            .set(&PhoneKey::OwnerDetails, &(owner, name));
     }
 
-    pub fn create_new_phone_book(env: Env, phone_book_name: String, salt: BytesN<32>, wasm_hash: BytesN<32>, constructor_args: Vec<Val>,) {
+    pub fn create_new_phone_book(
+        env: Env,
+        phone_book_name: String,
+        salt: BytesN<32>,
+        wasm_hash: BytesN<32>,
+        constructor_args: Vec<Val>,
+    ) {
         let owner: Address = env.storage().instance().get(&PhoneKey::Owner).expect("msg");
         owner.require_auth();
-        let phone_book_address: Address = env.deployer().with_address(owner.clone(), salt).deploy_v2(wasm_hash, constructor_args);
+        let phone_book_address: Address = env
+            .deployer()
+            .with_address(owner.clone(), salt)
+            .deploy_v2(wasm_hash, constructor_args);
 
-        // save it 
-        env.storage().instance().set(&PhoneKey::PhoneBookName(phone_book_name), &phone_book_address);
+        // save it
+        env.storage().instance().set(
+            &PhoneKey::PhoneBookName(phone_book_name),
+            &phone_book_address,
+        );
     }
 
-    pub fn add_new_contact(env: Env, phone_book_name: String, contact_info: ContactInfo ) {
+    pub fn add_new_contact(env: Env, phone_book_name: String, contact_info: ContactInfo) {
         let owner: Address = env.storage().instance().get(&PhoneKey::Owner).expect("msg");
         owner.require_auth();
 
-        let phone_book_address: Address = env.storage().instance().get(&PhoneKey::PhoneBookName(phone_book_name)).expect("msg");
+        let phone_book_address: Address = env
+            .storage()
+            .instance()
+            .get(&PhoneKey::PhoneBookName(phone_book_name))
+            .expect("msg");
 
         let phone_book_instance: PhoneBookClient = PhoneBookClient::new(&env, &phone_book_address);
 
         phone_book_instance.add_contact(&contact_info.name, &contact_info.no);
     }
 
-    pub fn get_contact(env: Env, phone_book_name: String, contact_name: String) -> Option<phone_book::ContactInfo>{
+    pub fn get_contact(
+        env: Env,
+        phone_book_name: String,
+        contact_name: String,
+    ) -> Option<phone_book::ContactInfo> {
         let owner: Address = env.storage().instance().get(&PhoneKey::Owner).expect("msg");
         owner.require_auth();
 
-        let phone_book_address: Address = env.storage().instance().get(&PhoneKey::PhoneBookName(phone_book_name)).expect("msg");
+        let phone_book_address: Address = env
+            .storage()
+            .instance()
+            .get(&PhoneKey::PhoneBookName(phone_book_name))
+            .expect("msg");
 
-                let phone_book_instance: PhoneBookClient = PhoneBookClient::new(&env, &phone_book_address);
+        let phone_book_instance: PhoneBookClient = PhoneBookClient::new(&env, &phone_book_address);
 
-            phone_book_instance.get_contact(&contact_name)
-            
-
+        phone_book_instance.get_contact(&contact_name)
     }
 }
--- contracts/phone_book/src/lib.rs
+++ contracts/phone_book/src/lib.rs
@@ -1,18 +1,15 @@
 #![no_std]
 
-use soroban_sdk::{contract, contractimpl, contracttype, Address, Env, String,};
+use soroban_sdk::{contract, contractimpl, contracttype, Address, Env, String};
 
 mod test;
-
-
 
 #[contracttype]
 pub enum PhoneBookKey {
     Owner,
     Name,
-    Contact(String,),
+    Contact(String),
 }
-
 
 #[contracttype]
 #[derive(Debug)]
@@ -24,10 +21,8 @@ pub struct ContactInfo {
 #[contract]
 pub struct PhoneBook;
 
-
 #[contractimpl]
 impl PhoneBook {
-
     pub fn __init(env: &Env, owner: Address, name: String) {
         if env.storage().instance().has(&PhoneBookKey::Owner) {
             // throw
@@ -37,14 +32,14 @@ impl PhoneBook {
         env.storage().instance().set(&PhoneBookKey::Name, &name);
     }
 
-
     pub fn add_contact(env: &Env, name: String, mobile_number: String) {
-
         let contact_info = ContactInfo {
             name: name.clone(),
-            mobile_number
+            mobile_number,
         };
-        env.storage().instance().set(&PhoneBookKey::Contact(name), &contact_info);
+        env.storage()
+            .instance()
+            .set(&PhoneBookKey::Contact(name), &contact_info);
     }
 
     pub fn get_contact(env: Env, name: String) -> Option<ContactInfo> {
@@ -52,27 +47,42 @@ impl PhoneBook {
     }
 
     pub fn remove_contact(env: Env, name: String) {
-        let owner: Address = env.storage().instance().get(&PhoneBookKey::Owner)
+        let owner: Address = env
+            .storage()
+            .instance()
+            .get(&PhoneBookKey::Owner)
             .expect("Contract not initialized");
         owner.require_auth();
 
-        let is_exist:bool = env.storage().instance().has(&PhoneBookKey::Contact(name.clone()));
-        if !is_exist {
-            
-        }
-        
-        env.storage().instance().remove(&PhoneBookKey::Contact(name));
+        let is_exist: bool = env
+            .storage()
+            .instance()
+            .has(&PhoneBookKey::Contact(name.clone()));
+        if !is_exist {}
+
+        env.storage()
+            .instance()
+            .remove(&PhoneBookKey::Contact(name));
     }
 
     pub fn update_contact(env: Env, name: String, new_mobile: String) {
-        let owner: Address = env.storage().instance().get(&PhoneBookKey::Owner)
+        let owner: Address = env
+            .storage()
+            .instance()
+            .get(&PhoneBookKey::Owner)
             .expect("Contract not initialized");
         owner.require_auth();
 
-        let mut contact_info: ContactInfo = env.storage().instance().get(&PhoneBookKey::Contact(name.clone())).expect(""); 
+        let mut contact_info: ContactInfo = env
+            .storage()
+            .instance()
+            .get(&PhoneBookKey::Contact(name.clone()))
+            .expect("");
 
         contact_info.mobile_number = new_mobile;
 
-        env.storage().instance().set(&PhoneBookKey::Contact(name), &contact_info);
+        env.storage()
+            .instance()
+            .set(&PhoneBookKey::Contact(name), &contact_info);
     }
 }
